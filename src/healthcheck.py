7c5071383d32ff42073b2a84d3638727cfdf2d9a
tracebacker <dsrase201@gmail.com>
2026-04-16 00:58:24 +0300

    Добавил LoggerFactory для удобного логирования

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -1,6 +1,8 @@
 import requests as req
+from src.logger import LoggerFactory
 
 def healthcheck():
+    logger = LoggerFactory.get_logger(__name__)
     try:
         response = req.get('https://google.com/', timeout=5)
         if response.status_code == 200:
@@ -8,4 +10,5 @@ def healthcheck():
         else:
             return False
     except req.exceptions.RequestException:
+        logger.error("Request провалился. Нестабильное интернет соединение.")
         return False
---
+++ src/logger/LoggerFactory.py
@@ -0,0 +1,21 @@
+import logging
+
+class LoggerFactory:
+    _configured = False
+
+    @classmethod
+    def _configure(cls):
+        if cls._configured:
+            return
+
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        cls._configured = True
+
+    @classmethod
+    def get_logger(cls, name: str) -> logging.Logger:
+        cls._configure()
+        return logging.getLogger(name)
