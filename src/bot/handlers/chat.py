65313a70ea8ad41fe91d558f36f086bdd9106f0c
tracebacker <dsrase201@gmail.com>
2026-04-16 20:06:58 +0300

    добавил темку чтоб id вывелся

--- src/bot/handlers/chat.py
+++ src/bot/handlers/chat.py
@@ -6,4 +6,4 @@ router = Router()
 
 @router.message()
 async def echo(message: Message):
-    await message.answer(message.text)
+    await message.answer(message.text + f"\nYour id is {message.from_user.id}")
